ac2c3198c6c9ea0838037517e93562a2d8c788c2
chldmswnl12 <chldmswnl12@gmail.com>
2021-05-01 17:41:21 +0900

    card css

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import styles from "./card.module.css";
 
-const Card = ({ card }) => (
-  <section className={styles.card}>
-    <img
-      src="../../../public/images/default_logo.png"
-      alt="image"
-      className={styles.cardImg}
-    />
-    <div className={styles.cardInfo}>
-      <p className={styles.name}>{card.name}</p>
-      <p className={styles.info}>{card.company}</p>
-      <hr className={styles.line} />
-      <p className={styles.info}>{card.title}</p>
-      <p className={styles.info}>{card.email}</p>
-      <p className={styles.info}>{card.message}</p>
-    </div>
-  </section>
-);
+const DEFAULT_IMAGE = "/images/default_logo.png";
+const Card = ({ card }) => {
+  const {
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL,
+  } = card;
+  const url = fileURL || DEFAULT_IMAGE;
+  return (
+    <section className={`${styles.card} ${getStyles(theme)}`}>
+      <img src={url} alt="image" className={styles.cardImg} />
+      <div className={styles.cardInfo}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.company}>{company}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.email}>{email}</p>
+        <p className={styles.message}>{message}</p>
+      </div>
+    </section>
+  );
+};
+
+function getStyles(theme) {
+  switch (theme) {
+    case "dark":
+      return styles.dark;
+    case "light":
+      return styles.light;
+    case "colorful":
+      return styles.colorful;
+    default:
+      throw new Error(`unknown theme: ${theme}`);
+  }
+}
 
 export default Card;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -3,28 +3,47 @@ import styles from "./maker.module.css";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import { useHistory } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CardInfo from "../cardInfo/cardInfo";
 import CardPreview from "../cardPreview/cardPreview";
 
 const Maker = ({ authService }) => {
   const history = useHistory();
-  const cards = [
+  const [cards, setCards] = useState([
     {
+      id: "1",
       name: "Eunji Choi",
       company: "Cineplex",
+      theme: "light",
       title: "manager",
       email: "chldmswnl12@gmail.com",
       message: "Work hard",
+      fileName: "ellie",
+      fileURL: null,
     },
     {
+      id: "2",
       name: "Sam perez",
       company: "CRA",
       title: "CEO",
+      theme: "dark",
       email: "sammy@gmail.com",
       message: "Eat a lot",
+      fileName: "ellie",
+      fileURL: null,
     },
-  ];
+    {
+      id: "3",
+      name: "Sam perez",
+      company: "CRA",
+      title: "CEO",
+      theme: "colorful",
+      email: "sammy@gmail.com",
+      message: "Eat a lot",
+      fileName: "ellie",
+      fileURL: null,
+    },
+  ]);
   const onLogout = () => {
     authService.logout();
   };
